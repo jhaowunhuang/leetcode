d03f664b3458ca502f4f570077165199c9062027
dynamicy <chris@csie.io>
2020-03-26 12:59:03 +0800

    Refactor 0001 & add rules for lint.

--- 0001-Two_Sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ref = {}
-        for i, item in enumerate(nums):
-            if target - item in ref:
-                return [ref[target - item], i]
-            ref[item] = i
