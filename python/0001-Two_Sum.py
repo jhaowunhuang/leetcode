96612ea0a55c1becedac4595377d7aaa0ac1a95a
Chris <chrishuang@noodoe.com>
2020-03-27 00:46:04 +0800

    refactor 0001 ~ 0006.

--- python/0001-Two_Sum.py
+++ python/0001-Two_Sum.py
@@ -1,10 +1,7 @@
-def twoSum(nums, target):
-    ref = {}
-    for i, item in enumerate(nums):
-        if target - item in ref:
-            return [ref[target - item], i]
-        ref[item] = i
-
-
 class Solution:
-    pass
+    def twoSum(nums, target):
+        ref = {}
+        for i, item in enumerate(nums):
+            if target - item in ref:
+                return [ref[target - item], i]
+            ref[item] = i
--- python/0002-Add_Two_Numbers.py
+++ python/0002-Add_Two_Numbers.py
@@ -5,21 +5,21 @@ class ListNode:
         self.next = None
 
 
-def addTwoNumbers(l1, l2):
-    ans = node = ListNode(0)
-    a = 0
-    while l1 or l2 or a:
-        if l1:
-            a += l1.val
-            l1 = l1.next
-        if l2:
-            a += l2.val
-            l2 = l2.next
-        a, b = divmod(a, 10)
-        node.next = ListNode(b)
-        node = node.next
-    return ans.next
-
-
 class Solution:
-    pass
+    def __init__(self):
+        self.val = None
+
+    def addTwoNumbers(l1, l2):
+        ans = node = ListNode(0)
+        a = 0
+        while l1 or l2 or a:
+            if l1:
+                a += l1.val
+                l1 = l1.next
+            if l2:
+                a += l2.val
+                l2 = l2.next
+            a, b = divmod(a, 10)
+            node.next = ListNode(b)
+            node = node.next
+        return ans.next
