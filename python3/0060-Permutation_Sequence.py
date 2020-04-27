71fa47c9fca76546b7b4d6b9b557da1c1b81e60f
jhaowunhuang <jhaowunhuang@gmail.com>
2020-04-27 14:46:42 +0800

    add new file

---
+++ python3/0060-Permutation_Sequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getPermutation(self, n, k):
+        ref = [str(x) for x in range(1, n + 1)]
+        fac = math.factorial(n)
+        ans = ''
+        for i in range(n, 0, -1):
+            fac //= i
+            r, k = divmod(k, fac)
+            ans += ref.pop(r if k != 0 else r - 1)
+        return ans
